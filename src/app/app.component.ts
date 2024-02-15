a17ee32d9f1f26b20a34e740784aece2af695618
pablo7843 <dewislewis8@gmail.com>
2024-02-15 11:43:31 +0100

    Frontent
    Frontend enviado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,26 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
+<<<<<<< Updated upstream
 import { HttpClient } from '@angular/common/http';
 import { TableroComponent } from './components/tablero/tablero.component';
+=======
+import { StorageService } from './services/storage.service';
+import { AuthService } from './services/auth.service';
+import { TableroComponent } from './components/tablero/tablero.component';
+import { AddPlayerComponent } from './components/add-player/add-player.component';
+
+>>>>>>> Stashed changes
 
 HttpClient
 @Component({
   selector: 'app-root',
   standalone: true,
+<<<<<<< Updated upstream
   imports: [RouterOutlet,LoginComponent,TableroComponent],
+=======
+  imports: [RouterOutlet, LoginComponent, TableroComponent, AddPlayerComponent],
+>>>>>>> Stashed changes
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
